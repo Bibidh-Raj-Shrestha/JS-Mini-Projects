099f4d795602c76c477a02c8f0e559c127474476
Bibidh-Raj-Shrestha <bibidhrajshrestha54321@gmail.com>
2025-12-31 19:09:36 +0545

    added fuctionality to next-btn

--- Quiz-App/script.js
+++ Quiz-App/script.js
@@ -1,3 +1,9 @@
+let quiz = document.getElementById("quiz");
+let question = document.getElementById("question");
+let options = document.getElementById("options");
+let next_btn = document.getElementById("next-btn");
+let score = document.getElementById("score");
+
 const questions = [
     {
         question: "What is 2 + 2?",
@@ -10,3 +16,48 @@ const questions = [
         answer: "JavaScript"
     }
 ];
+
+next_btn.addEventListener("click",next_question);
+
+let index=0;
+function next_question(){
+
+    const selected = document.querySelector('input[name="quiz-option"]:checked');
+
+    if (selected) {
+        console.log(selected.value);
+    }
+
+
+
+    
+    
+
+    render_question(); 
+    index++;
+}
+
+function render_question(){
+    // extracting current qeustion from the array 
+    q = questions[index];
+
+    question.textContent = q.question;
+
+    //clearing the options before redering new ones
+    options.innerHTML ="";
+    //options rendering
+    q.options.forEach(option => {
+        let div = document.createElement("div");
+        let input = document.createElement("input");
+        input.type = "radio";
+        input.name = "quiz-opt.";
+        input.value = option;
+
+        let label = document.createElement("label");
+        label.textContent = option;
+
+        div.appendChild(input);
+        div.appendChild(label);
+        options.appendChild(div);
+    });
+}
