99a91349e71f0401a2b2c3862470ffbccdc10b52
Bibidh-Raj-Shrestha <bibidhrajshrestha54321@gmail.com>
2025-12-31 18:25:48 +0545

    Added basic skeleton of quiz app

---
+++ Quiz-App/script.js
@@ -0,0 +1,12 @@
+const questions = [
+    {
+        question: "What is 2 + 2?",
+        options: ["1", "2", "4", "5"],
+        answer: "4"
+    },
+    {
+        question: "JS stands for?",
+        options: ["Java Source", "JavaScript", "Just Script"],
+        answer: "JavaScript"
+    }
+];
