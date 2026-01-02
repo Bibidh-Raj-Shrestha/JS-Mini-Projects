7315fefac661a81ea10fac0b4ccef5cba03eece5
Bibidh-Raj-Shrestha <bibidhrajshrestha54321@gmail.com>
2026-01-02 08:04:47 +0545

    Added the pause and restart functions

--- Countdown-Timer/script.js
+++ Countdown-Timer/script.js
@@ -1,6 +1,8 @@
 // inputs
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
+minutes.value = "";
+seconds.value = "";
 
 // display
 const time_display = document.getElementById("time-display");
@@ -21,21 +23,56 @@ reset_btn.addEventListener("click",reset);
 // time variables
 
 let total_secs = 0;
+let check = null;
 
 function start(){
-    let mins = Number(minutes.value);
-    let secs = Number(seconds.value);
-    console.log(mins,secs);
+    if(check == null){
+        let mins = Number(minutes.value);
+        let secs = Number(seconds.value);
+        minutes.value = "";
+        seconds.value = "";
+
+        total_secs = (mins*60)+secs;
+        check = setInterval(update_timer,1000);
+    }
 }
 
+let pause_check =  false;
 function pause(){
-
+    if(!pause_check){
+        check = clearInterval(check);
+        pause_check = true;
+    }
+    else{
+        check = setInterval(update_timer,1000);
+        pause_check = false;
+    }
+    
 }
 
 function reset(){
-     
+    check = clearInterval(check);
+    update_display(0);
+     return;
 }
 
-function update_timer(total_sec){
+function update_timer(){
 
+    if(total_secs<=0)
+    {
+        clearInterval(check);
+        check = null;
+        return;
+    }
+
+    total_secs--;
+    update_display(total_secs);
+    
+}
+
+function update_display(total_sec)
+{
+    let mins = Math.floor(total_sec/60);
+    let secs = total_sec % 60;
+    time_display.textContent = `${String(mins).padStart(2,0)}:${String(secs).padStart(2,0)}`;
 }
