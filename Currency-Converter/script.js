51b203ffd2b81db8ca9ede85e9be99dc44c16d8c
Bibidh-Raj-Shrestha <bibidhrajshrestha54321@gmail.com>
2026-01-08 20:54:54 +0545

    added validation to base and targeted currency

--- Currency-Converter/script.js
+++ Currency-Converter/script.js
@@ -5,6 +5,7 @@ const currencyT = document.getElementById("currencyT");
 const result = document.getElementById("result");
 const convert = document.getElementById("convert");
 const reset = document.getElementById("reset");
+const message = document.getElementById("message");
 
 // btn function
 convert.addEventListener("click",convert_Currency);
@@ -30,13 +31,35 @@ async function convert_Currency(){
 // api call for currecy rates
 async function getCurrency(base_currency,to_currency,amt){
     try{
+        message.innerText = "";
         let response = await fetch(`https://api.exchangerate-api.com/v4/latest/${base_currency}`)
+
+        if(!response.ok)
+        {
+            message.innerText = "Please enter valid base currency code!";
+            return 0;
+        }
+
         let data =await response.json();
         let data_rate = await data.rates[`${to_currency}`];
+        if(data_rate === undefined)
+        {
+            message.innerText = "Please enter valid targeted currency code!";    
+            return 0;
+        }
         return (amt*data_rate);
     }
     catch(error)
     {
+
         console.log(error);
     }
 }
+
+// changes the other input to uppercase
+currencyF.addEventListener("input",()=>{
+    currencyF.value = currencyF.value.toUpperCase(); 
+});
+currencyT.addEventListener("input",()=>{
+    currencyT.value = currencyT.value.toUpperCase(); 
+});
