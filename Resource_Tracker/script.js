a9c543f850a81e830b0bd63d4a26030cb0b378a7
Bibidh-Raj-Shrestha <bibidhrajshrestha54321@gmail.com>
2026-01-13 12:08:12 +0545

    added localstorage and delete button

--- Resource_Tracker/script.js
+++ Resource_Tracker/script.js
@@ -1,5 +1,7 @@
 // ======= 1. State =======
-let resources = []; // Array to hold all resources
+let items = localStorage.getItem("items");
+let resources = (items)? JSON.parse(items) : [];
+
 
 // ======= 2. DOM Elements =======
 const form = document.getElementById('resource-form');
@@ -7,6 +9,8 @@ const titleInput = document.getElementById('title');
 const typeSelect = document.getElementById('type');
 const statusSelect = document.getElementById('status');
 const resource_list = document.getElementById('resource-list');
+
+renderResources();
 
 // ======= 3. Event Listener (Form Submit) =======
 form.addEventListener('submit', (e) => {
@@ -28,7 +32,7 @@ form.addEventListener('submit', (e) => {
     // 3c. Push into resources array
     resources.push(resource_obj);
 
-    // 3d. Clear input fields (form.reset() works nicely)
+    //reset form
     form.reset();
 
     renderResources();
@@ -37,33 +41,58 @@ form.addEventListener('submit', (e) => {
 // ======= 4. Render Function =======
 function renderResources() {
     // Clear resourceList container 
-    resource_list.innerHTML = '';
+    resource_list.innerHTML = '';   
 
-    
+    let itemstring = JSON.stringify(resources);
+    localStorage.setItem("items",itemstring);
 
     // Loop over resources array
     resources.forEach((el,index) => {
 
+        // S.N
         let no = document.createElement("h4");
         no.textContent = ++index+".";
 
+        // Title
         let title = document.createElement("p");
         title.textContent = el.title;
 
+        // Type
         let type = document.createElement("p");
         type.textContent = el.type;
 
+        // Status
         let status = document.createElement("p");
         status.textContent = el.status;
 
+        // Actions 
+        let action_div = document.createElement("div");
+        action_div.classList.add("action-div");
+        let edit_btn = document.createElement('button');
+        edit_btn.textContent = "Edit";
+        let del_btn = document.createElement("button");
+        del_btn.value = el.id ;
+        del_btn.textContent = "Delete";
+        del_btn.addEventListener("click",()=>{
+            delete_resource(del_btn.value);
+        });
+        
+        action_div.append(edit_btn,del_btn);
+
+        // resource cards
         let resource_card = document.createElement("div");
         resource_card.classList.add("resource-card");
-        resource_card.append(no,title,type,status);
+        resource_card.append(no,title,type,status,action_div);
 
+        // append to resource list
         resource_list.appendChild(resource_card);
     });
+}
 
-    // Create DOM element for each resource (plain text: Title | Type | Status)
-
-    // Append to resourceList
+function delete_resource(target){
+    let req_resources = resources.filter((resource)=>{
+        return resource.id != target;
+    });
+    resources = req_resources;
+    renderResources();
 }
