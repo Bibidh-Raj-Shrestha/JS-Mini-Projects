7137784983727dcdd37b62f373b65ceccc3adc41
Bibidh-Raj-Shrestha <bibidhrajshrestha54321@gmail.com>
2026-01-16 10:24:49 +0545

    Polish Display-Mode-Toggle: root variables, smooth transitions, persistence, icon toggle, hover effect

--- Display-Mode-Toggle/script.js
+++ Display-Mode-Toggle/script.js
@@ -1,26 +1,60 @@
+// main variables
 const toggle = document.getElementById("toggle");
 const toggle_ball = document.getElementById("toggle-ball");
 const sun = document.getElementById("sun");
 const moon = document.getElementById("moon");
 
-toggle.addEventListener("click",()=>
-{
-    if(toggle_ball.classList.contains("toggle-ball-on"))
+// get previous theme
+let theme = localStorage.getItem("theme");
+let current_theme = (theme === "dark")? "dark" : "light";
+initializer();
+
+// toggle function call 
+toggle.addEventListener("click",toggle_theme);
+
+// main function to toggle theme decides which theme
+function toggle_theme(){    
+    if(current_theme === "dark")
     {
-        toggle_ball.classList.remove("toggle-ball-on");
-        toggle_ball.classList.add("toggle-ball-off");
-        toggle.classList.toggle("toggle-on");
-        document.body.style.backgroundColor="white";
-        moon.style.display = "none";
-        sun.style.display = "block";
+        current_theme = "light";
+        light_theme();  
     }
     else{
-        toggle_ball.classList.remove("toggle-ball-off");
-        toggle_ball.classList.add("toggle-ball-on");
-        toggle.classList.toggle("toggle-on");
-        document.body.style.backgroundColor="black";
-        sun.style.display = "none";
-        moon.style.display = "block";
+        current_theme = "dark";
+        dark_theme();
     }
-    
-});
+    localStorage.setItem("theme",current_theme);
+}
+
+// converts to light theme
+function light_theme(){
+    toggle.classList.remove("toggle-on");
+    toggle_ball.classList.remove("toggle-ball-on");
+    toggle_ball.classList.add("toggle-ball-off");    
+    document.documentElement.style.setProperty('--bg-color',"white");
+    document.documentElement.style.setProperty('--shadow-color',"black");
+    moon.style.display = "none";
+    sun.style.display = "block";
+}
+
+// converts to dark theme
+function dark_theme(){    
+    toggle.classList.add("toggle-on");
+    toggle_ball.classList.remove("toggle-ball-off");
+    toggle_ball.classList.add("toggle-ball-on");
+    document.documentElement.style.setProperty('--bg-color',"black");
+    document.documentElement.style.setProperty('--shadow-color',"white");
+    sun.style.display = "none";
+    moon.style.display = "block";
+}
+
+// initializes based on localstorage
+function initializer(){
+    if(current_theme === "dark")
+    {
+        dark_theme();  
+    }
+    else{
+        light_theme();
+    }
+}
